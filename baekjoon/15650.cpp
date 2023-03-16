0c3fd823ea2780e29c2c13bab53868b5cdc5dd1f
mzeong <70924556+mzeong@users.noreply.github.com>
2023-03-16 13:09:47 +0900

    N과 M (2) | 백트래킹

---
+++ baekjoon/15650.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m; 
+int arr[10];
+
+void f(int k) {
+    if (k == m) {
+        for (int i = 0; i < m; i++) {
+            cout << arr[i] << ' ';
+        }
+        cout << '\n';
+        return;
+    }
+    
+    for (int i = arr[k-1] + 1; i <= n; i++) {
+        arr[k] = i;
+        f(k+1);
+        arr[k] = 0;
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(0); cin.tie(0);
+    
+    cin >> n >> m;
+    f(0);
+    return 0;
+}
