0e6e2216f159e2189d2c10c4a0b4fbe49f6edb32
mzeong <70924556+mzeong@users.noreply.github.com>
2023-10-26 00:08:05 +0900

    List of Unique Numbers

--- Algorithm/baekjoon/Two-Pointers/13144.java
+++ Algorithm/baekjoon/Two-Pointers/13144.java
@@ -6,50 +6,27 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n + 1];
+        int[] sequence = new int[n];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 1; i <= n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < n; i++) {
+            sequence[i] = Integer.parseInt(st.nextToken());
         }
 
-        int l = 1;
-        int r = 0;
+        boolean[] exist = new boolean[100001];
         long answer = 0;
-        int[] ct = new int[100_001];
-        while (l <= n) {
-            while (r + 1 <= n && ct[arr[r + 1]] == 0) {
+        int l = 0;
+        int r = 0;
+        while (l < n) {
+            while (r < n && !exist[sequence[r]]) {
+                exist[sequence[r]] = true;
                 r++;
-                ct[arr[r]]++;
             }
-
-            answer += r - l + 1;
-            ct[arr[l++]]--;
+            answer += r - l;
+            exist[sequence[l]] = false;
+            l++;
         }
-
         System.out.println(answer);
-//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//
-//        int n = Integer.parseInt(br.readLine());
-//        int[] sequence = new int[n + 1];
-//        StringTokenizer st = new StringTokenizer(br.readLine());
-//        for (int i = 1; i <= n; i++) {
-//            sequence[i] = Integer.parseInt(st.nextToken());
-//        }
-//
-//        boolean[] exist = new boolean[100001];
-//        int answer = 0;
-//        int l = 1;
-//        int r = 0;
-//        while (l <= n) {
-//            while (r + 1 <= n && !exist[sequence[r + 1]]) {
-//                r++;
-//                exist[sequence[r]] = true;
-//            }
-//            answer += r - l + 1;
-//            exist[sequence[l]] = false;
-//            l++;
-//        }
-//        System.out.println(answer);
     }
 }
