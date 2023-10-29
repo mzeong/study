47dd47ff8333d1bd6714deb94118c88375d96d6d
mzeong <70924556+mzeong@users.noreply.github.com>
2023-10-29 18:22:59 +0900

    Create 재구매가 일어난 상품과 회원 리스트 구하기.sql

---
+++ MYSQL/programmers/재구매가 일어난 상품과 회원 리스트 구하기.sql
@@ -0,0 +1,7 @@
+-- 동일한 회원이 동일한 상품을 재구매한 데이터를 구하여, 재구매한 회원 ID와 재구매한 상품 ID를 출력하는 SQL문을 작성
+-- 결과는 회원 ID를 기준으로 오름차순 정렬, 회원 ID가 같다면 상품 ID를 기준으로 내림차순 정렬
+SELECT USER_ID, PRODUCT_ID
+FROM ONLINE_SALE
+GROUP BY USER_ID, PRODUCT_ID
+HAVING COUNT(*) > 1
+ORDER BY USER_ID, PRODUCT_ID DESC
