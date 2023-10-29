20e5bc18a94bfaa6fabcf4aff053bb1b5adcef5e
mzeong <70924556+mzeong@users.noreply.github.com>
2023-10-29 18:25:21 +0900

    Create 과일로 만든 아이스크림 고르기.sql

---
+++ MYSQL/programmers/과일로 만든 아이스크림 고르기.sql
@@ -0,0 +1,10 @@
+-- 상반기 아이스크림 총주문량이 3,000보다 높으면서
+-- 아이스크림의 주 성분이 과일인 
+-- 아이스크림의 맛을 
+-- 총주문량이 큰 순서대로 조회
+SELECT FIRST_HALF.FLAVOR
+FROM FIRST_HALF
+JOIN ICECREAM_INFO 
+ON FIRST_HALF.FLAVOR = ICECREAM_INFO.FLAVOR AND INGREDIENT_TYPE = 'fruit_based'
+WHERE TOTAL_ORDER > 3000
+ORDER BY TOTAL_ORDER DESC
