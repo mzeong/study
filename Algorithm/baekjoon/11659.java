cc5f00d2c6bc520cce7f4dddfe742d54f98f868c
mzeong <mzeongdev@gmail.com>
2024-05-26 22:57:32 +0900

    [추가] 구간 합 구하기 4

---
+++ Algorithm/baekjoon/11659.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        int[] arr = new int[n + 1];
+        int[] dp = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            dp[i] = dp[i - 1] + arr[i];
+        }
+
+        while (m-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            int i = Integer.parseInt(st.nextToken());
+            int j = Integer.parseInt(st.nextToken());
+
+            int answer = dp[j] - dp[i - 1];
+            System.out.println(answer);
+        }
+    }
+}
