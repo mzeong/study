9f9405ef3fdb2b1db4f9a6cf768151c7cda3208e
mzeong <mzeongdev@gmail.com>
2024-08-19 00:56:15 +0900

    [추가] 카드 구매하기

---
+++ Algorithm/baekjoon/11052.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] p = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            p[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] dp = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            dp[i] = p[i];
+        }
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j < i; j++) {
+                dp[i] = Math.max(dp[i], dp[j] + dp[i - j]);
+            }
+        }
+        System.out.println(dp[n]);
+    }
+}
